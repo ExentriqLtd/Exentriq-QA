0f6b3ede730bfd96d1a5b4602492c6c39d26a46f
stoufa88 <stoufa.turki@gmail.com>
2015-11-11 11:59:16 +0100

    Export user id in comments

--- server/rest.js
+++ server/rest.js
@@ -7,3 +7,18 @@ var Api = new Restivus({
 Api.addCollection(Posts);
 Api.addCollection(Comments);
 
+
+// Maps to: /custom-api/users/:name
+  Api.addRoute('users/:name', {authRequired: false}, {
+    get: function () {
+    	const user = Users.findOne({'profile.name':this.urlParams.name });
+    	if(user)
+	      return {
+	      	_id: user._id,
+	      	username: user.username,
+	      	profile: user.profile
+	      }
+	    else
+	    	return 'error: user not found';
+    }
+  });
