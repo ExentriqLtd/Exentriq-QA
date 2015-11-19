9e69315895e9638a0198ccb3d168b51a1581af4b
stoufa88 <stoufa.turki@gmail.com>
2015-11-19 17:29:49 +0100

    Document + refactor restful

--- server/rest.js
+++ server/rest.js
@@ -4,8 +4,20 @@ var Api = new Restivus({
   apiPath: 'custom-api'
 });
 
-// Maps to: /custom-api/users/:name
-Api.addRoute('users/:name', {authRequired: false}, {
+/**
+ * @api {get} /user/:name Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {String} name Users unique Name.
+ *
+ * @apiSuccess {String} _id ID of the User.
+ * @apiSuccess {String} username  Username of the User.
+ * @apiSuccess {Object} profile  Profile of the User.
+ *
+ * @apiError UserNotFound The name of the User was not found.
+ */
+Api.addRoute('user/:name', {authRequired: false}, {
   get: function () {
   	const user = Users.findOne({'profile.name':this.urlParams.name });
   	if(user)
@@ -15,29 +27,83 @@ Api.addRoute('users/:name', {authRequired: false}, {
       	profile: user.profile
       }
     else
-    	return 'error: user not found';
+    	return {
+        error: 'User not found' 
+      };
   }
 });
 
-// Posts collection routes
-Api.addRoute('posts/', {authRequired: false}, {
+/**
+ * @api {get} /posts Request posts collection
+ * @apiName GetPosts
+ * @apiGroup Post
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
+/**
+ * @api {post} /posts Create new post
+ * @apiName CreatePost
+ * @apiGroup Post
+ * 
+ * @apiParam {String} title Title of the Post.
+ * @apiParam {String} [body] Body of the Post.
+ * @apiParam {Number} status Status of the Post (1 - 2 - 3).
+ * @apiParam {Boolean} [internal] Type of the Post. Public or Internal.
+ *
+ * @apiSuccess {String} _id ID of the post.
+ * @apiSuccess {String} title Title of the post.
+ * @apiSuccess {Number} status Status of the post.
+ * @apiSuccess {Number} commentsCount Number of comments of the post.
+ * @apiSuccess {Boolean} internal Type of the post.
+ * @apiSuccess {String} userId User or author of the post.
+ */
+Api.addRoute('posts', {authRequired: false}, {
   get: function(){
     return {
       status: "success",
-      data: Posts.find({}).fetch()
+      data: Posts.find({}, {fields: {
+        'sticky': 0,
+        'postedAt': 0,
+        'slug': 0,
+        'htmlBody': 0 }
+      }).fetch()
     }
   },
   post: function(){
     var id = Posts.insert(this.bodyParams);
     return {
       status: "success",
-      data: {
-        _id: id
-      }
+      _id: id
     }
   }
 });
 
+/**
+ * @api {patch} /posts Update a post
+ * @apiName UpdatePost
+ * @apiGroup Post
+ * 
+ * @apiParam {String} id ID of the Post.
+ *
+ * @apiParam {String} title Title of the Post.
+ * @apiParam {String} [body] Body of the Post.
+ * @apiParam {Number} status Status of the Post (1 - 2 - 3).
+ * @apiParam {Boolean} [internal] Type of the Post. Public or Internal.
+ *
+ * @apiSuccess {String} _id ID of the post.
+ */
+
+ /**
+ * @api {posts} /posts Delete a post
+ * @apiName DeletePost
+ * @apiGroup Post
+ * 
+ * @apiParam {String} id ID of the Post.
+ * 
+ * @apiSuccess {String} _id ID of the post.
+ */
 Api.addRoute('posts/:id', {authRequired: false}, {
   patch: function () {
     // remove all comments for this post
@@ -51,9 +117,13 @@ Api.addRoute('posts/:id', {authRequired: false}, {
     });
     return {
       status: "success",
-      data: {
-        _id: this.urlParams.id
-      }
+      _id: this.urlParams.id
+    }
+  },
+  delete: function(){
+    Posts.remove(this.urlParams.id);
+    return {
+      status: "success"
     }
   }
 });
