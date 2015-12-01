015e7094e1f700b55ebe6e02569bd445956c98d9
stoufa88 <stoufa.turki@gmail.com>
2015-12-01 14:43:04 +0100

    Fix back url

--- client/config.js
+++ client/config.js
@@ -17,20 +17,19 @@ Template.registerHelper('isStandalone', function(){
 });
 
 Template.registerHelper('backToEmaUrl', function(){
-  if(FlowRouter.getQueryParam('view') === 'support' ||
-      FlowRouter.getRouteName() !== 'postsDefault')
+  if(FlowRouter.getQueryParam('view') === 'support')
     return false;
 	var postId = FlowRouter.getParam('_id');
 	var post = Posts.findOne(postId);
-	if(post && post.backToMyRoots){
+	if(post && post.backToMyRoots) {
     // We persist the back url in the session to enable back to ema link in app wide
     Session.set('post.backToMyRoots', post.backToMyRoots);
-		return post.backToMyRoots;
-  }else if(Session.get('post.backToMyRoots')){
-  	return Session.get('post.backToMyRoots');
-  }else{
-    return false;
+		return false;
   }
+  if(FlowRouter.getRouteName() == 'postsDefault' && Session.get('post.backToMyRoots')){
+    return Session.get('post.backToMyRoots');
+  }
+
 });
 
 // setup a new user spaceid for every new login
