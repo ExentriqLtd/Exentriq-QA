d081aef8e8ff8f11bd2fcca79dc9ff760219c082
Mustapha <mustapha@emiketic.com>
2015-12-03 18:54:10 +0100

    Add custom route for create user

--- server/rest.js
+++ server/rest.js
@@ -177,3 +177,24 @@ Api.addRoute('user/:name', {authRequired: false}, {
       };
   }
 });
+
+// create user
+Api.addRoute('users/createUser', {authRequired: false}, {
+  post: function() {
+    try {
+      var platformUsername = this.bodyParams.username;
+      var platformEmail = this.bodyParams.email;
+      Meteor.call('registerPlatformUser', platformUsername, platformEmail);
+
+      return {
+        status: 'success'
+      };
+    }
+    catch(e) {
+      return {
+        statusCode: 400,
+        body: {status: 'fails', message: e.name + '::' + e.message}
+      };
+    }
+  }
+});
