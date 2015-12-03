54b9dac0fef0ee5359fb72494b964007f5e3f85a
Mustapha <mustapha@emiketic.com>
2015-12-03 18:34:35 +0100

    Added from account to rest qa request

--- server/methods.js
+++ server/methods.js
@@ -17,6 +17,7 @@ Meteor.methods({
             title: post.title,
             description: post.body || '',
             spaceId: spaceId,
+            fromAccount: user.username,
             //public: true,
             members: []
           }
@@ -31,53 +32,53 @@ Meteor.methods({
     }
   },
 
-  sendNotification: function(doc, type){
-    check(doc.title, String);
-    check(doc._id, String);
-    check(doc.userId, String);
+  // sendNotification: function(doc, type){
+  //   check(doc.title, String);
+  //   check(doc._id, String);
+  //   check(doc.userId, String);
 
-    var from = Meteor.users.findOne(doc.userId).username
-    var to  = '';
-    var action = '';
-    if(type === TYPE_POST)
-      action = 'Created a new post';
-    if(type === TYPE_COMMENT)
-      action = 'Commented on a post';
-    var subject = from + ' ' + action + ' '+ doc.title;
-    var link = '/posts/' + doc._id;
+  //   var from = Meteor.users.findOne(doc.userId).username
+  //   var to  = '';
+  //   var action = '';
+  //   if(type === TYPE_POST)
+  //     action = 'Created a new post';
+  //   if(type === TYPE_COMMENT)
+  //     action = 'Commented on a post';
+  //   var subject = from + ' ' + action + ' '+ doc.title;
+  //   var link = '/posts/' + doc._id;
 
-    try {
-      var result = HTTP.call("POST",
-        Meteor.settings.public.integration_bus_url + '/createEvent', {
-          data: {
-            event: "Notification",
-            id: "",
-            entities:[
-              {
-                name:"Notification",
-                value: {
-                  from: from,
-                  to: to,
-                  subject: subject,
-                  message: '',
-                  picture: Meteor.settings.public.avatar_path + from + '.jpg',
-                  link: link
-                }
-              }
-            ]
-          }
-        });
+  //   try {
+  //     var result = HTTP.call("POST",
+  //       Meteor.settings.public.integration_bus_url + '/createEvent', {
+  //         data: {
+  //           event: "Notification",
+  //           id: "",
+  //           entities:[
+  //             {
+  //               name:"Notification",
+  //               value: {
+  //                 from: from,
+  //                 to: to,
+  //                 subject: subject,
+  //                 message: '',
+  //                 picture: Meteor.settings.public.avatar_path + from + '.jpg',
+  //                 link: link
+  //               }
+  //             }
+  //           ]
+  //         }
+  //       });
 
-      console.log(result)
+  //     console.log(result)
 
-      return true;
-    }catch(e){
-      // Got a network error, time-out or HTTP error in the 400 or 500 range.
-      console.error("error", e);
-      console.log("error", "cannot save notification to integration bus");
-      return false;
-    }
-  },
+  //     return true;
+  //   }catch(e){
+  //     // Got a network error, time-out or HTTP error in the 400 or 500 range.
+  //     console.error("error", e);
+  //     console.log("error", "cannot save notification to integration bus");
+  //     return false;
+  //   }
+  // },
 
   registerPlatformUser: function(username, email) {
     userId = Accounts.createUser({username: username, email: email, password:'exentriq'});
